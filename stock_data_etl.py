e56512db7a297e6c98ae74520f667f29134048eb
Nely <nely@Marianas-MacBook-Pro.local>
2023-04-20 15:55:01 -0700

    fix: bugs, add requirements.

--- stock_data_etl.py
+++ stock_data_etl.py
@@ -1,6 +1,8 @@
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 from yahoo_stock_data import get_stock_data
+from google.cloud import bigquery
+import pandas as pd
 
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
@@ -8,36 +10,37 @@ default_args = {
     'owner': 'airflow',
 }
 
-@dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2))
+@dag(default_args=default_args, schedule=None, start_date=days_ago(2))
 
 def stock_data_etl():
     @task()
     def extract():
-        get_stock_data("googl")
-        return order_data_dict
-    @task(multiple_outputs=True)
-    def transform(order_data_dict: dict):
-        """
-        #### Transform task
-        A simple Transform task which takes in the collection of order data and
-        computes the total order value.
-        """
-        total_order_value = 0
+        return get_stock_data("msft", "2022-04-20", "2022-04-21")
 
-        for value in order_data_dict.values():
-            total_order_value += value
-
-        return {"total_order_value": total_order_value}
     @task()
-    def load(total_order_value: float):
-        """
-        #### Load task
-        A simple Load task which takes in the result of the Transform task and
-        instead of saving it to end user review, just prints it out.
-        """
+    def transform(stock_data: pd.DataFrame) -> pd.DataFrame:
+        return stock_data
 
-        print("Total order value is: %.2f" % total_order_value)
-    order_data = extract()
-    order_summary = transform(order_data)
-    load(order_summary["total_order_value"])
-tutorial_etl_dag = tutorial_taskflow_api_etl()
+    @task()
+    def load(transformed_stock_data: pd.DataFrame) -> None:
+        # Construct a BigQuery client object.
+        client = bigquery.Client()
+
+        job_config = bigquery.LoadJobConfig()
+
+        table_id = "stock_data_microsoft"
+
+        job = client.load_table_from_dataframe(
+        transformed_stock_data, table_id, job_config=job_config
+        )  # Make an API request.
+
+        job.result()  # Wait for the job to complete.
+
+
+    stock_data = extract()
+
+    transformed_stock_data = transform(stock_data)
+
+    load(transformed_stock_data)
+
+stock_data_etl_dag = stock_data_etl()
