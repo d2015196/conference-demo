ea967e8d23e8cf88e2d4e27c38362df8f9135b8b
Nely <nely@Marianas-MacBook-Pro.local>
2023-04-20 16:31:31 -0700

    fix: start_date, end_date use.

--- yahoo_stock_data.py
+++ yahoo_stock_data.py
@@ -2,14 +2,15 @@
 import yfinance as yf
 from datetime import datetime
 from typing import Dict
+import pandas as pd
 
-def get_stock_data(company: str, start_date: str, end_date: str) -> Dict: 
+def get_stock_data(company: str, start_date: str, end_date: str) -> pd.DataFrame: 
     """
     Returns stock data broken down by minute for argument company and time period. Note that the requested date range i.e. [start_date, end_date] must be within the last 30 days. This is a yahoo finance API restriction.
         Args:
            company: Company for which data is desired (e.g. 'MSFT')
-            start_date: Start of time period
-            end_date: End of time period 
+            start_date: Start of time period in format YYYY-MM-DD
+            end_date: End of time period in format YYYY-MM-DD
         Returns:
            Stock data as a json object.
     """
@@ -19,7 +20,7 @@ def get_stock_data(company: str, start_date: str, end_date: str) -> Dict:
     
     return data
 
-stock_data = get_stock_data("msft", "2023-04-18", "2023-04-19")
+stock_data = get_stock_data("msft", start_date, end_date)
 
-print(stock_data)
+print(type(stock_data))
 
